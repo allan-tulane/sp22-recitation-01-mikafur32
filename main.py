cde960e5b2d3733cabeb8e6589a493dd5e331fd9
mikafur32 <msison1@tulane.edu>
2022-02-11 12:31:02 -0600

    Finished BS. TODO: compare searche

--- main.py
+++ main.py
@@ -14,6 +14,9 @@ def linear_search(mylist, key):
 			return i
 	return -1
 
+
+
+
 def test_linear_search():
 	""" done. """
 	assert linear_search([1,2,3,4,5], 5) == 4
@@ -37,17 +40,23 @@ def _binary_search(mylist, key, left, right):
 	Returns:
 	  index of key in mylist, or -1 if not present.
 	"""
-	### TODO
+	mid = (left + right) // 2
+	if (mylist[mid] == key):
+		return mid
 
-	###
+	elif(left > right):
+		return -1
+	elif (key < mylist[mid]):
+		return _binary_search(mylist, key, left, mid - 1)
+	else:
+		return _binary_search(mylist, key, mid + 1, right)
 
 def test_binary_search():
 	assert binary_search([1,2,3,4,5], 5) == 4
 	assert binary_search([1,2,3,4,5], 1) == 0
 	assert binary_search([1,2,3,4,5], 6) == -1
-	### TODO: add two more tests here.
-
-	###
+	assert binary_search([1,2,3,4,5], 3) == 2
+	assert binary_search([1,2,3,4,5], 2) == 1
 
 
 def time_search(search_fn, mylist, key):
@@ -68,9 +77,10 @@ def time_search(search_fn, mylist, key):
 	  the number of milliseconds it takes to run this
 	  search function on this input.
 	"""
-	### TODO
-
-	###
+	start = time.time()
+	search_fn(mylist,key)
+	finish = time.time()
+	return (finish - start) * 1000
 
 def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
 	"""
@@ -87,9 +97,7 @@ def compare_search(sizes=[1e1, 1e2, 1e3, 1e4, 1e5, 1e6, 1e7]):
 	  indicating the number of milliseconds it takes
 	  for each method to run on each value of n
 	"""
-	### TODO
-
-	###
+	return [len(sizes)-1, time_search(linear_search, sizes, -1), time_search(binary_search, sizes, -1)]
 
 def print_results(results):
 	""" done """
@@ -105,3 +113,4 @@ def test_compare_search():
 	assert res[1][0] == 100
 	assert res[0][1] < 1
 	assert res[1][1] < 1
+
